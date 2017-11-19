a8f2ab53961633900f14824e75a247f1d945cdc8
tomdol <tdolbniak+npmjs@gmail.com>
2017-11-19 12:12:02 +0100

    Use const instead of let and change the salt generation to async version

--- index.js
+++ index.js
@@ -1,39 +1,45 @@
 'use strict'
 
-let bcrypt = require('bcrypt');
-let crypto = require('crypto');
+const bcrypt = require('bcrypt');
+const crypto = require('crypto');
 
-let DEFAULT_CIPHER = 'aes256';
+const DEFAULT_CIPHER = 'aes256';
 
 module.exports = {
     /**
-     * Encrypts the given input with bcrypt + selected cipher.
-     * @param  {[string]} input        Data to encrypt. bcrypt expects input to be a string.
-     * @param  {[string]} password     Password used to encrypt the hashed data. The same password should be used in compare function.
+     * Hashes and encrypts the given input with bcrypt + selected cipher.
+
+     * @param  {[string]} input        Data to hash & encrypt. bcrypt expects input to be a string.
+     * @param  {[string]} password     Password used to encrypt the hashed data. The same password should be used in compare function for verification.
      * @param  {[number]} bcryptRounds Number of key expansion rounds in bcrypt. Bigger values provide better security of encrypted data but also take more time to compute the hash.
      * @param  {[string]} cipherType   One of cipher types available in nodejs's crypto module. By default aes256 is used. Call crypto.getCiphers() to check available ciphers.
-     * @return {[Promise]}             Returns a Promise object. On success the resolve function is called with one param containing the encrypted input.
+     * @return {[Promise]}             On success the returned promise resolves with the hashed and encrypted input.
      */
     encrypt: function(input, password, bcryptRounds, cipherType) {
         return new Promise(function(resolve, reject) {
             try {
                 if(typeof password !== 'string' || (cipherType && typeof cipherType !== 'string')) {
-                    throw new Error('password and cipherType must be strings');
+                    throw new Error('Both password and cipherType must be strings');
                 }
 
                 if(!input || input.length === 0) {
-                    throw new Error('No input data');
+                    throw new Error('The input data cannot be empty');
                 }
 
-                let salt = bcrypt.genSaltSync(bcryptRounds);
+                const salt = null;
+                try {
+                    salt = await bcrypt.genSalt(bcryptRounds);
+                } catch(saltGenerationError) {
+                    throw new Error('Could not generate bcrypt salt. ' + saltGenerationError.toString());
+                }
 
                 bcrypt.hash(input, salt, function(bcerror, hashedData) {
                     if(bcerror) {
                         throw bcerror;
                     } else {
-                        let cipher = crypto.createCipher(cipherType || DEFAULT_CIPHER, password);
-                        let enc1 = cipher.update(hashedData, 'utf8', 'hex');
-                        let enc2 = cipher.final('hex');
+                        const cipher = crypto.createCipher(cipherType || DEFAULT_CIPHER, password);
+                        const enc1 = cipher.update(hashedData, 'utf8', 'hex');
+                        const enc2 = cipher.final('hex');
 
                         resolve(enc1 + enc2);
                     }
@@ -46,6 +52,7 @@ module.exports = {
 
     /**
      * The function decrypts encryptedData using the provided password. Decrypted data is then compared with input to see if the hashes match.
+
      * @param  {[string]} input         Input data to compare with decrypted hash.
      * @param  {[string]} encryptedData Hash encrypted with password and cipherType.
      * @param  {[string]} password      Password used to decrypt the encryptedData.
@@ -63,15 +70,15 @@ module.exports = {
                     throw new Error('No input data');
                 }
 
-                let decipher = crypto.createDecipher(cipherType || DEFAULT_CIPHER, password);
-                let dec1 = decipher.update(encryptedData, 'hex', 'utf8');
-                let dec2 = decipher.final('utf8');
+                const decipher = crypto.createDecipher(cipherType || DEFAULT_CIPHER, password);
+                const dec1 = decipher.update(encryptedData, 'hex', 'utf8');
+                const dec2 = decipher.final('utf8');
 
-                let decrypted = dec1 + dec2;
+                const decrypted = dec1 + dec2;
 
-                bcrypt.compare(input, decrypted, function(bcerror, result) {
-                    if(bcerror) {
-                        throw bcerror;
+                bcrypt.compare(input, decrypted, function(bcryptError, result) {
+                    if(bcryptError) {
+                        throw bcryptError;
                     } else {
                         resolve(result);
                     }
