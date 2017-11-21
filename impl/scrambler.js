aed3da51781f1586637f0d6f9f077ab01783291e
tomdol <tdolbniak+npmjs@gmail.com>
2017-11-21 17:41:54 +0100

    Return promises from scrambler's methods

--- impl/scrambler.js
+++ impl/scrambler.js
@@ -2,20 +2,28 @@
 
 const crypto = require('crypto');
 
+const encrypt = (input, password, cipherType) => {
+    const cipher = crypto.createCipher(cipherType, password);
+    const enc1 = cipher.update(input, 'utf8', 'hex');
+    const enc2 = cipher.final('hex');
+
+    return enc1 + enc2;
+};
+
+const decrypt = (data, password, cipherType) => {
+    const decipher = crypto.createDecipher(cipherType, password);
+    const dec1 = decipher.update(data, 'hex', 'utf8');
+    const dec2 = decipher.final('utf8');
+
+    return dec1 + dec2;
+};
+
 module.exports = {
     encrypt: (input, password, cipherType) => {
-        const cipher = crypto.createCipher(cipherType, password);
-        const enc1 = cipher.update(input, 'utf8', 'hex');
-        const enc2 = cipher.final('hex');
-
-        return enc1 + enc2;
+        return Promise.resolve(encrypt(input, password, cipherType));
     },
 
     decrypt: (data, password, cipherType) => {
-        const decipher = crypto.createDecipher(cipherType, password);
-        const dec1 = decipher.update(data, 'hex', 'utf8');
-        const dec2 = decipher.final('utf8');
-
-        return dec1 + dec2;
+        return Promise.resolve(decrypt(data, password, cipherType));
     }
 };
